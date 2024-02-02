afc15543e6396697e53486f44f7b5dda29a7f001
sengpablo <sengineerpablo@gmail.com>
2024-02-02 20:06:05 -0300

    novo layout dashboard

---
+++ static/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+	<?php
+		include 'header.php'
+	?>
+
+<body>
+	<div class="wrapper">
+        <?php include 'menu.php' ?>
+
+
+		<div class="main">
+        <?php include 'topo.php' ?>
+
+        <?php include 'corpo.php' ?>
+
+
+<footer class="footer">
+    <?php include 'footer.php' ?>
+				
+</footer>
+		</div>
+	</div>
+
+	<script src="js/app.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
+

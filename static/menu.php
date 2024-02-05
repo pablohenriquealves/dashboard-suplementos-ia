a8747254be00c58d93818425726ba6f40cfbe281
Jullyane <jullyaneflmdigital@gmail.com>
2024-02-05 19:42:50 -0300

    mudança no layout

--- static/menu.php
+++ static/menu.php
@@ -1,108 +1,108 @@
-<nav id="sidebar" class="sidebar js-sidebar">
-	<!--bg-danger altera cor da sidebar na div abaixo-->
-			<div class="sidebar-content js-simplebar">
-				<a class="sidebar-brand" href="index.php">
-          <span class="align-middle">Suplementos IA</span>
-          <i class="fa-solid fa-dumbbell"></i>
-                </a>
+<head>
+	<?php include 'header.php'?>
 
-				<ul class="sidebar-nav">
-					<li class="sidebar-header">
-						Pages
-					</li>
+</head>
 
-					<li class="sidebar-item active">
-						<a class="sidebar-link" href="index.php">
-              <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
-            </a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages-profile.html">
-              <i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
-            </a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages-sign-in.html">
-              <i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Sign In</span>
-            </a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages-sign-up.html">
-              <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Sign Up</span>
-            </a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="pages-blank.html">
-              <i class="align-middle" data-feather="book"></i> <span class="align-middle">Blank</span>
-            </a>
-					</li>
-
-					<li class="sidebar-header">
-						Tools & Components
-					</li>
-<!--bg-danger altera cor do item no li abaixo-->
-					<li class="sidebar-item">
-						<a class="sidebar-link " href="ui-buttons.html">
-              <i class="align-middle" data-feather="square"></i> <span class="align-middle">Buttons</span>
-            </a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-forms.html">
-              <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Forms</span>
-            </a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-cards.html">
-              <i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
-            </a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-typography.html">
-              <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Typography</span>
-            </a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="icons-feather.html">
-              <i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
-            </a>
-					</li>
-
-					<li class="sidebar-header">
-						Plugins & Addons
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="charts-chartjs.html">
-              <i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Charts</span>
-            </a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="maps-google.html">
-              <i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
-            </a>
-					</li>
-				</ul>
-
-				<div class="sidebar-cta">
-					<!-- bg-danger altera cor do banner na div abaixo -->
-					<div class="sidebar-cta-content">
-						<strong class="d-inline-block mb-2">Upgrade to Pro</strong>
-						<div class="mb-3 text-sm">
-							Are you looking for more components? Check out our premium version.
-						</div>
-						<div class="d-grid">
-							<a href="upgrade-to-pro.html" class="btn btn-primary">Upgrade to Pro</a>
+<body>
+	<nav id="sidebar" class="sidebar js-sidebar">
+		<!--bg-danger altera cor da sidebar na div abaixo-->
+				<div class="sidebar-content js-simplebar bg-teal-400">
+					<a class="sidebar-brand" href="index.php">
+	          <span class="align-middle">Suplementos IA</span>
+	          <i class="fa-solid fa-dumbbell"></i>
+	                </a>
+	
+					<ul class="sidebar-nav ">
+						<li class="sidebar-header">
+							Pages
+						</li>
+	
+						<li class="sidebar-item bg-teal-400">
+							<a class="sidebar-link" href="formcliente.php">
+	              <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Clientes</span>
+	            </a>
+						</li>
+	
+						<li class="sidebar-item">
+							<a class="sidebar-link " href="formproduto.php">
+	              <i class="align-middle" data-feather="user"></i> <span class="align-middle">Produto</span>
+	            </a>
+						</li>
+	
+						<li class="sidebar-item">
+							<a class="sidebar-link" href="pages-sign-in.html">
+	              <i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Fornecedor</span>
+	            </a>
+						</li>
+	
+						<li class="sidebar-item">
+							<a class="sidebar-link" href="pages-sign-up.html">
+	              <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Sign Up</span>
+	            </a>
+						</li>
+	
+						<li class="sidebar-item">
+							<a class="sidebar-link" href="pages-blank.html">
+	              <i class="align-middle" data-feather="book"></i> <span class="align-middle">Blank</span>
+	            </a>
+						</li>
+	<!--bg-danger altera cor do item no li abaixo-->
+						<li class="sidebar-item ">
+							<a class="sidebar-link " href="ui-buttons.html">
+	              <i class="align-middle" data-feather="square"></i> <span class="align-middle">Buttons</span>
+	            </a>
+						</li>
+	
+						<li class="sidebar-item">
+							<a class="sidebar-link" href="ui-forms.html">
+	              <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Forms</span>
+	            </a>
+						</li>
+	
+						<li class="sidebar-item">
+							<a class="sidebar-link" href="ui-cards.html">
+	              <i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
+	            </a>
+						</li>
+	
+						<li class="sidebar-item">
+							<a class="sidebar-link" href="ui-typography.html">
+	              <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Typography</span>
+	            </a>
+						</li>
+	
+						<li class="sidebar-item">
+							<a class="sidebar-link" href="icons-feather.html">
+	              <i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
+	            </a>
+						</li>
+	
+						<li class="sidebar-item">
+							<a class="sidebar-link" href="charts-chartjs.html">
+	              <i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Charts</span>
+	            </a>
+						</li>
+	
+						<li class="sidebar-item">
+							<a class="sidebar-link" href="maps-google.html">
+	              <i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
+	            </a>
+						</li>
+					</ul>
+	
+					<div class="sidebar-cta">
+						<!-- bg-danger altera cor do banner na div abaixo -->
+						<div class="sidebar-cta-content">
+							<strong class="d-inline-block mb-2">Upgrade to Pro</strong>
+							<div class="mb-3 text-sm">
+								Are you looking for more components? Check out our premium version.
+							</div>
+							<div class="d-grid">
+								<a href="upgrade-to-pro.html" class="btn btn-primary">Upgrade to Pro</a>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</nav>
+			</nav>
+	
+</body>

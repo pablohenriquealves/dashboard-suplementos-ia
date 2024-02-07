f5dd19db39a6a0402f62f3dc0b2e0f0df5036fd3
Jullyane <jullyaneflmdigital@gmail.com>
2024-02-07 20:36:14 -0300

    config do formcliente

--- static/formproduto.php
+++ static/formproduto.php
@@ -2,22 +2,7 @@
 <html lang="pt-br">
 
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
-	<meta name="author" content="AdminKit">
-	<meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
-
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link rel="shortcut icon" href="img/icons/icon-48x48.png" />
-
-	<link rel="canonical" href="https://demo-basic.adminkit.io/pages-blank.html" />
-
-	<title>Blank Page | AdminKit Demo</title>
-
-	<link href="css/app.css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+	<?php include 'header.php'; ?>
 </head>
 
 <body>
@@ -25,7 +10,7 @@
 		<nav id="sidebar" class="sidebar js-sidebar">
 			<div class="sidebar-content js-simplebar">
 				<a class="sidebar-brand" href="index.html">
-          <span class="align-middle">Cadastro de clientes</span>
+          <span class="align-middle">Suplementos IA</span>
         </a>
 
 				<ul class="sidebar-nav">
--- static/topo.php
+++ static/topo.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand navbar-light navbar-bg">
 				<a class="sidebar-toggle js-sidebar-toggle">
-          <i class="hamburger align-self-center"></i>
+          <i class="hamburger align-self-center ms-2"></i>
         </a>
 
 				<div class="navbar-collapse collapse">
